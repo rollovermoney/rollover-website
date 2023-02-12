e8040655d858be28303c649f9737b36d768f52a6
shaurya127 <shaurya1272000@gmail.com>
2023-02-12 16:03:53 +0530

    seo metatag added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import BusinessSteps from './components/BusinessSteps';
 import Mobile from './components/Mobile';
 import Footer from './components/Footer';
 import FAQ from './components/FAQ';
+import {Helmet} from "react-helmet";
 const App = () => {
   return (
     <div className="App">
+
+
+      <Helmet>
+        
+        <title>rollover</title>
+        <meta name="description" content="RollOver Money is a unique invoice and receivable discounting platform. Investors can invest in an invoice basket, similar to mutual funds and REITs. Small businesses can receive financing with ease, while investors earn attractive returns. Join us in revolutionizing the traditional lending process by investing in RollOverMoney today." />
+      </Helmet>
       <Navbar />
       <BannerSection />
   
