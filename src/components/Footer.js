d9f990eab272c5e881d6ffdb6096133ad85d2ec6
shaurya127 <shaurya1272000@gmail.com>
2023-03-22 12:22:15 +0530

    footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <>
     
     <div class="pg-footer">
-    <footer class="footer">
+    <footer class="footer" >
       <svg class="footer-wave-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 100" preserveAspectRatio="none">
         <path class="footer-wave-path" d="M851.8,100c125,0,288.3-45,348.2-64V0H0v44c3.7-1,7.3-1.9,11-2.9C80.7,22,151.7,10.8,223.5,6.3C276.7,2.9,330,4,383,9.8 c52.2,5.7,103.3,16.2,153.4,32.8C623.9,71.3,726.8,100,851.8,100z"></path>
       </svg>
@@ -95,9 +95,7 @@ const Footer = () => {
             <h2 class="footer-call-to-action-title"> You Call Us</h2>
             <p class="footer-call-to-action-link-wrapper"> <a class="footer-call-to-action-link" href="tel:0124-64XXXX" target="_self">9650830027</a></p>
           </div>
-          
         </div>
-       
       </div>
       <div class="footer-copyright">
         <div class="footer-copyright-wrapper">
