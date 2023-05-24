79b4a5f47d26c213337ed086905ab66774db9302
shaurya127 <shaurya1272000@gmail.com>
2023-05-24 18:11:06 +0530

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,76 +4,68 @@ import CustomForm from "./newsletter";
 const Footer = () => {
   return (
     <>
-    
-      <div
-        className="container-fluid pt-3 px-md-5"
-        style={{ background: " #121139" }}
-      >
+      <div className="container-fluid pt-3 px-md-5" style={{ background: "#121139" }}>
         <div className="row">
           <div className="col-md-12">
-            <img height="50px" src="/images/Logo.svg"></img>
+            <img height="50px" src="/images/Logo.svg" alt="Logo" />
           </div>
-          <div className="col-sm-1"></div>
-          <div className="col-sm-10 p-sm-5">
-            <div className="d-flex footercontent">
-              {/* <div className="col">
-                <p>Services</p>
-                <ul>
-                  <li>Invoice Discounting</li>
-                  <li> Receivable Discounting</li>
-                  <li>Invoicing SaaS Platform</li>
-                  <li>Pay Later (coming soon)</li>
-                </ul>
-              </div> */}
-              <div className="col">
-                <p>Support</p>
-                <ul>
-                  <li>Pricing Plan</li>
-                  <li>Documentation</li>
-                  <li>Guide</li>
-                  <li>Tutorial</li>
-                </ul>
-              </div>
-              <div className="col">
-                <p>Company</p>
-                <ul>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-170434">
-                <a href="/contact-us" target="_blank" style={{textDecoration:"none",color:"white"}}>Contact Us</a>
-              </li>
-                  <li>About</li>
-                  <li>Blog</li>
-                  
-                 
-                </ul>
-              </div>
-              <div className="col">
-                <p>Legal</p>
-                <ul>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-170434">
-                <a href="/privacy-policy" target="_blank" style={{textDecoration:"none",color:"white"}}>Privacy Policy</a>
-              </li>
-              <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                <a href="/terms-condition" target="_blank" style={{textDecoration:"none",color:"white"}}>Terms & Conditions</a>
-              </li>
-              <li class="menu-item menu-item-type-post_type menu-item-object-page">
-                <a href="/cancellation-policy" target="_blank" style={{textDecoration:"none",color:"white"}}>Cancellation policy</a>
-              </li>
-                </ul>
-              </div>
-              <div className="col" >
-                <p>Subcribe to our newsletter</p>
-                <ul>
-                  <li>Be ahead of your competitors by efficiently managing your working capital</li>
-                </ul>
-                {/* <CustomForm /> */}
-              </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <div className="footercontent">
+              <p>Services</p>
+              <ul>
+                <li>Invoice Discounting</li>
+                <li>Receivable Discounting</li>
+                <li>Invoicing SaaS Platform</li>
+              </ul>
             </div>
           </div>
-          <div className="col-sm-1"></div>
+          <div className="col-md-3 col-sm-6">
+            <div className="footercontent">
+              <p>Support</p>
+              <ul>
+                <li>Pricing Plan</li>
+                <li>Documentation</li>
+                <li>Guide</li>
+                
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="footercontent">
+              <p>Company</p>
+              <ul>
+                <li><a href="/contact-us" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Contact Us</a></li>
+                <li>About</li>
+                <li>Blog</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <div className="footercontent">
+              <p>Legal</p>
+              <ul>
+                <li><a href="/privacy-policy" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Privacy Policy</a></li>
+                <li><a href="/terms-condition" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Terms & Conditions</a></li>
+                <li><a href="/cancellation-policy" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Cancellation policy</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+       
+        <div className="row">
+          <div className="col-md-12">
+            <div className="footercontent" style={{marginLeft:"auto",marginRight:"auto",marginTop:"20px"}}>
+              <p style={{fontSize:"12px",textAlign:"center"}}>&copy; 2023 Your Company. All Rights Reserved.</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
+
+
 
 export default Footer;
